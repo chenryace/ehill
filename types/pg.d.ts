7b95082fdedabfdb0d59c2571df90a4fdfdeb55a
chenryace <78716224+chenryace@users.noreply.github.com>
2025-04-12 23:46:40 +0800

    Update pg.d.ts

--- types/pg.d.ts
+++ types/pg.d.ts
@@ -1,1 +1,38 @@
-declare module 'pg';
+declare module 'pg' {
+  export class Pool {
+    constructor(config?: any);
+    connect(): Promise<PoolClient>;
+    query(text: string, params?: any[]): Promise<QueryResult>;
+    end(): Promise<void>;
+  }
+
+  export class Client {
+    constructor(config?: any);
+    connect(): Promise<void>;
+    query(text: string, params?: any[]): Promise<QueryResult>;
+    end(): Promise<void>;
+  }
+
+  export interface PoolClient {
+    query(text: string, params?: any[]): Promise<QueryResult>;
+    release(err?: Error): void;
+  }
+
+  export interface QueryResult {
+    rows: any[];
+    rowCount: number;
+    command: string;
+    oid: number;
+    fields: FieldInfo[];
+  }
+
+  export interface FieldInfo {
+    name: string;
+    tableID: number;
+    columnID: number;
+    dataTypeID: number;
+    dataTypeSize: number;
+    dataTypeModifier: number;
+    format: string;
+  }
+}
