f8f5e15d66c90cc4237721340ae243350668acf4
chenryace <78716224+chenryace@users.noreply.github.com>
2025-04-14 11:23:28 +0800

    Update editor.tsx

--- components/editor/editor.tsx
+++ components/editor/editor.tsx
@@ -67,7 +67,7 @@ const Editor: FC<EditorProps> = memo(({ readOnly: propReadOnly, isPreview }) => 
             saveNote?.();
             toast('笔记已保存', 'success');
         }
-    }, { enableOnTags: true }, [isEditing, saveNote, toast]);
+    }, { enableOnTags: ['INPUT', 'TEXTAREA', 'SELECT'] }, [isEditing, saveNote, toast]);
     
     useHotkeys('ctrl+e, cmd+e', (e) => {
         e.preventDefault();
