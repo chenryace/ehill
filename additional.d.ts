e580a1ad823ed7ed9fe239d9147f15b57544219f
chenryace <78716224+chenryace@users.noreply.github.com>
2025-04-15 10:32:34 +0800

    Update additional.d.ts

--- additional.d.ts
+++ additional.d.ts
@@ -7,3 +7,8 @@ declare module 'outline-icons';
 declare module '@notea/headway-widget';
 
 declare module 'markdown-link-extractor';
+
+// 添加全局Window接口扩展
+interface Window {
+  __EDIT_MODE_TOGGLE__?: boolean;
+}
