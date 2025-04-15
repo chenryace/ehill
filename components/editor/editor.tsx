6179aa22adc847f981638ee265c6b8358ef5f6ad
chenryace <78716224+chenryace@users.noreply.github.com>
2025-04-16 03:57:35 +0800

    Update editor.tsx

--- components/editor/editor.tsx
+++ components/editor/editor.tsx
@@ -7,7 +7,6 @@ import {
     useRef,
     useEffect, 
 } from 'react';
-import { searchNote, searchRangeText } from 'libs/web/utils/search';
 import useFetcher from 'libs/web/api/fetcher';
 import { NOTE_DELETED } from 'libs/shared/meta';
 import { isNoteLink, NoteModel } from 'libs/shared/note';
