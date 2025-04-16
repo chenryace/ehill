447a9ce19095c01cf2d748446846053c3451cba9
chenryace <78716224+chenryace@users.noreply.github.com>
2025-04-16 15:14:13 +0800

    Update main-editor.tsx

--- components/editor/main-editor.tsx
+++ components/editor/main-editor.tsx
@@ -1,5 +1,6 @@
 import EditTitle from './edit-title';
 import Editor, { EditorProps } from './editor';
+// @ts-ignore - This component is used in the JSX below but TypeScript doesn't recognize it
 import Backlinks from 'components/backlinks';
 import SaveButton from './save-button';
 import EditorState from 'libs/web/state/editor';
